c5a8c8793b41ec54b45989dfd16a6802f9ed1f5d
kanikabansal95 <106958369+kanikabansal95@users.noreply.github.com>
2022-09-21 00:21:58 +0530

    Create playing with characters.c

---
+++ assignment 2/playing with characters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+ char a[100];
+ scanf ("%[^\n]%*c",a);
+ printf(a);
+ printf ("\n");
+ char b[80];
+ scanf ("%[^\n]%*c",b);
+ printf (b);
+ printf("\n");
+ char c[80];
+  scanf ("%[^\n]%*c",c);
+  printf(c);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
